900d5edea1b734fa02ebfc60983cfe6ffa4cde2f
Cameron Haygood <cameronhaygood@gmail.com>
2025-07-07 19:40:52 -0700

    Users can no be displayed

--- api.py
+++ api.py
@@ -13,7 +13,7 @@ api = Api(app)
 
 class UserRecord(db.Model):
 
-    __tablename__ = 'usertable'
+    __tablename__ = "usertable"
     user_id = db.Column(db.String, primary_key=True)
     first_name = db.Column(db.String)
     last_name = db.Column(db.String)
